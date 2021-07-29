63e60993f216f04b58a6e35defeecc7dbfd4baf7
Adonys Santos <adonyssantosjimenez@gmail.com>
2021-07-28 22:30:09 -0400

    Add props

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import HolaMundo from './components/HolaMundo';
+import Button from './components/Button';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<HolaMundo />, document.getElementById('root'));
+ReactDOM.render(<Button text='Jajajaj' />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
